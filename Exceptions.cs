423b77a05540433577e2c1a7bbf2383adf3d0bd3
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-14 13:44:33 +0100

    Change Exceptions.cs file
    There was some problem with previous commit

--- Exceptions.cs
+++ Exceptions.cs
@@ -1,4 +1,24 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿namespace TowerDefenceTreehouse
 {
-  public class $CLASS$ {$END$}
+    internal class TreehouseDefenseException : System.Exception
+    {
+        public TreehouseDefenseException()
+        {
+        }
+
+        public TreehouseDefenseException(string message) : base(message)
+        {
+        }
+    }
+
+    internal class OutOfBoundsException : TreehouseDefenseException
+    {
+        public OutOfBoundsException()
+        {
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+        }
+    }
 }
