9ee53aca3fce5d08fa30c599bed04741f297ab17
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-15 14:25:24 +0100

    Add check for bounds in constructor
    If MapLocation constructor is not on the Map, then OutOfBoundsException is thrown

--- MapLocation.cs
+++ MapLocation.cs
@@ -1,9 +1,17 @@
-﻿namespace TowerDefenceTreehouse
+
+namespace TowerDefenceTreehouse
 {
-    public class MapLocation : Point
+    internal class MapLocation : Point
     {
         public MapLocation(int x, int y, Map map) : base(x, y)
         {
+            if (!map.OnMap(this))
+            {
+                throw new OutOfBoundsException(x + ","
+                                               + y +
+                                               " is outside the boundaries of the map.");
+            }
         }
+        
     }
 }
