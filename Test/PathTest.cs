47be91cca95ceb56b634af9b5027f1dddc31a677
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-18 21:34:14 +0100

    Add PathTest with GetLocationAt success test
    Simple test that returned value is correct

---
+++ Test/PathTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace TowerDefenceTreehouse.Test
+{
+    [TestFixture]
+    public class PathTest
+    {
+        [Test]
+        public void GetMapLocationShouldReturnCorrectLocation()
+        {
+            // Given the Path with array of MapLocation
+            // with one "testMapLocation"
+            // initialized in constructor
+            Map map = new Map(10,10);
+            MapLocation testMapLocation =
+                new MapLocation(1,1, map);
+            Path path = new Path(
+                new [] {
+                    testMapLocation
+                }
+            );
+
+            // When GetLocationAt(0) is called
+            MapLocation returedMapLocation =
+                path.GetLocationAt(0);
+
+            // Then correct MapLocation is returned
+            Assert.AreEqual(testMapLocation, returedMapLocation);
+        }
+
+    }
+}
