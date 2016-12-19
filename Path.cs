ee823339da7bce71f91eb72a38f07ab2db6f5113
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-19 18:57:44 +0100

    Add GetLocationAt method
    that returns MapLocation at index of path array

--- Path.cs
+++ Path.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace TowerDefenceTreehouse
 {
     internal class Path
@@ -11,6 +13,25 @@ namespace TowerDefenceTreehouse
         public Path(MapLocation[] path)
         {
             _path = path;
+        }
+
+        /**
+        Gets {@code MapLocation} at given {@code pathStep}.
+        @param pathStep : index of element in MapLocation array
+        {@code _path} field.
+        @return MapLocation
+        @throws OutOfBoundsException : when {@code pathStep}
+        is out of bounds from {@code _path} field.
+        */
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            if (pathStep < 0 || pathStep > _path.Length - 1)
+            {
+                throw new OutOfBoundsException(
+                    "no MapLocation with this index found"
+                );
+            }
+            return _path[pathStep];
         }
     }
 }
