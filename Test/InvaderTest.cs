848d55583c8dec4f5ed3b391122c3686cf18a159
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-20 15:24:03 +0100

    Add InvaderTest class with "Move" method test
    Simply checks that upon calling "Move" "PathStep" is changed

---
+++ Test/InvaderTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace TowerDefenceTreehouse.Test
+{
+    [TestFixture]
+    public class InvaderTest
+    {
+        [Test]
+        public void MoveMethodShouldChangePathStep()
+        {
+            // Given Invader with some Path
+            Map map = new Map(10,10);
+            Path path = new Path(
+                new []
+                {
+                    new MapLocation(1,1, map),
+                }
+            );
+            Invader invader = new Invader(path);
+
+            // When Move() method is called
+            invader.Move();
+
+            // Then _pathStep should change to 1
+            Assert.AreEqual(1, invader.PathStep);
+        }
+
+    }
+}
