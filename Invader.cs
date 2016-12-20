2dd6e5c43718e2acb7e5cee31ca132e7c16cd7c6
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-20 14:59:43 +0100

    Add "pathStep" field to Invader.cs
    With initialization to 0 in default constructor

--- Invader.cs
+++ Invader.cs
@@ -3,10 +3,12 @@
     public class Invader
     {
         private readonly Path _path;
+        private int _pathStep;
 
         public Invader(Path path)
         {
             _path = path;
+            _pathStep = 0;
         }
     }
 }
