ca5bc9e9ed8afa13501a258f9b22382784e083d9
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-20 15:27:53 +0100

    Add comments to separate class into sections
    to "fields/member variables", "constructors" and "other methods"

--- Invader.cs
+++ Invader.cs
@@ -2,16 +2,22 @@
 {
     public class Invader
     {
+        // fields and properties
+
         private readonly Path _path;
         public int PathStep { get; private set; }
 
         public MapLocation Location => _path.GetLocationAt(PathStep);
+
+        // constructors
 
         public Invader(Path path)
         {
             _path = path;
             PathStep = 0;
         }
+
+        // other methods
 
         /**
         Moves Invader one step more.
