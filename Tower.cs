cf3b6e25fc0abaa061f775cda91b2d296bda5169
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-23 10:20:02 +0100

    Add static Random and MapLocation members

--- Tower.cs
+++ Tower.cs
@@ -1,4 +1,6 @@
-﻿namespace TowerDefenceTreehouse
+﻿using System;
+
+namespace TowerDefenceTreehouse
 {
     public class Tower
     {
@@ -7,6 +9,14 @@
         private const int Range = 1;
         private const int Power = 1;
         private const double Accuracy = .75;
+
+        // static members
+
+        private static readonly Random _random = new Random();
+
+        // members with class return type
+
+        private readonly MapLocation _location;
 
     }
 }
