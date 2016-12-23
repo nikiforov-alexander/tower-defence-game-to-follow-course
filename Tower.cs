6cdf7cc15fa37f121e753fd7a09855c0ac9c8589
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-23 10:17:13 +0100

    Add 3 const fields to Tower
    Range, Power and Accuracy

--- Tower.cs
+++ Tower.cs
@@ -2,6 +2,11 @@
 {
     public class Tower
     {
+        // constants
+
+        private const int Range = 1;
+        private const int Power = 1;
+        private const double Accuracy = .75;
 
     }
 }
