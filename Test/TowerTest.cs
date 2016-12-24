fcf6e8a9403b34b0ac0b92ebb53d15d1c3b577c7
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-24 20:36:19 +0100

    Add TowerTest class with first test
    The first test checks that shot is successful when we mock Random object passed
    to constructor of Tower class. Mocking is set in private method that takes one
    argument: value to be used as thrown by Random.nextDouble(). in [SetUp]
    method test Map is set to (5,5).
    
    RandomMock is used as a public member that is verified in tests and set up in
    private method

---
+++ Test/TowerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace TowerDefenceTreehouse.Test
+{
+    [TestFixture]
+    public class TowerTest
+    {
+        public Map TestMap;
+        public Tower TestTower;
+        public Mock<Random> RandomMock;
+
+        /// <summary>
+        /// here we set up only <c>TestMap</c>
+        /// 5
+        /// 4
+        /// 3
+        /// 2
+        /// 1   t
+        /// 0
+        ///   0 1 2 3 4 5
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            TestMap = new Map(5, 5);
+        }
+
+        /// <summary>
+        /// This method sets up <code>TestTower</code>
+        /// to be located at (1,1) position,
+        /// <see cref="TowerTest.SetUp"/>.
+        ///
+        /// It also mocks using Moq framework the <c>Random</c>
+        /// class that is passed to constructor of
+        /// <c>TestTower</c>
+        ///
+        /// </summary>
+        /// <param name="randomDoubleValue">
+        ///     This value is used to be compared with <c>Accuracy</c>
+        ///     in <c>Tower</c> class, <see cref="Tower.IsSuccessfulShot"/>
+        ///     for more.
+        /// </param>
+        private void SetUpTowerLocationWithRandomDoubleValue(double randomDoubleValue)
+        {
+            RandomMock = new Mock<Random>();
+
+            RandomMock.Setup(
+                rand => rand.NextDouble()
+            ).Returns(randomDoubleValue);
+
+            MapLocation towerLocation = new MapLocation(x: 1, y: 1, map: TestMap);
+
+            TestTower = new Tower(
+                location: towerLocation,
+                range: Tower.DefaultRange,
+                power: Tower.DefaultPower,
+                accuracy: Tower.DefaultAccuracy,
+                random: RandomMock.Object
+            );
+        }
+
+        [Test]
+        public void ShotOnInvaderIsSuccessfulWhenRandomGivesValueLessThanAccuracy()
+        {
+            // Given TestTower with HundredPercentShot,
+            // i.e. when Random gives 0.0, for Accuracy 0.75
+            SetUpTowerLocationWithRandomDoubleValue(0.0);
+
+            // When TestTower.isSuccesfulShot() is called
+
+            // Then true should be returned
+            Assert.True(TestTower.IsSuccessfulShot());
+
+            // Then _random.nextDouble should be called
+            RandomMock.Verify(random => random.NextDouble());
+        }
+
+    }
+}
