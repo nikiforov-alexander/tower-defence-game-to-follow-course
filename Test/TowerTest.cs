2eb16e1c5c99e68c3a1ac23094d55338ce209c16
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-25 10:04:55 +0100

    Add successful hit test on invader in TowerTest
    simply checks that if tower is in range of invader and shot will be accurate, then
    invader decreases health and "is hit" message is printed

--- Test/TowerTest.cs
+++ Test/TowerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Moq;
 using NUnit.Framework;
 
@@ -10,6 +11,7 @@ namespace TowerDefenceTreehouse.Test
         public Map TestMap;
         public Tower TestTower;
         public Mock<Random> RandomMock;
+        public Invader TestInvaderInTowerRange;
 
         /// <summary>
         /// here we set up only <c>TestMap</c>
@@ -61,6 +63,38 @@ namespace TowerDefenceTreehouse.Test
             );
         }
 
+        /// <summary>
+        /// Sets up the <c>TestInvader</c> that is in <c>TestTower.Range</c>
+        /// with <c>Invader.DefaultHealth</c> and 0 as <c>pathStep</c>
+        /// Here is how <c>invaderPath</c> looks like
+        /// 5
+        /// 4
+        /// 3
+        /// 2
+        /// 1   t
+        /// 0 i i i i i i
+        ///   0 1 2 3 4 5
+        /// </summary>
+        private void SetUpTestInvaderInTowerRange()
+        {
+            Path invaderPath = new Path(
+                new []
+                {
+                    new MapLocation(x:0, y:0, map: TestMap),
+                    new MapLocation(x:1, y:0, map: TestMap),
+                    new MapLocation(x:2, y:0, map: TestMap),
+                    new MapLocation(x:3, y:0, map: TestMap),
+                    new MapLocation(x:4, y:0, map: TestMap),
+                    new MapLocation(x:5, y:0, map: TestMap),
+                }
+            );
+            TestInvaderInTowerRange = new Invader(
+                path: invaderPath,
+                pathStep: 0,
+                health: Invader.DefaultHealth
+            );
+        }
+
         [Test]
         public void ShotOnInvaderIsSuccessfulWhenRandomGivesValueLessThanAccuracy()
         {
@@ -95,5 +129,45 @@ namespace TowerDefenceTreehouse.Test
             RandomMock.Verify(random => random.NextDouble());
         }
 
+
+        [Test]
+        public void ShotOnActiveInvaderInTowerRangeShouldHitAndDecreaseHealth()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                // Given TestTower with Hit Shot
+                SetUpTowerLocationWithRandomDoubleValue(0.0);
+                // Given Invader that is in range of TestTower
+                // 5
+                // 4
+                // 3
+                // 2
+                // 1   t
+                // 0 i i i i i i
+                //   0 1 2 3 4 5
+                SetUpTestInvaderInTowerRange();
+                Assert.IsTrue(TestInvaderInTowerRange.IsActive);
+                Assert.IsTrue(
+                    TestTower.IsInRangeOf(
+                        TestInvaderInTowerRange
+                    )
+                );
+                int testInvaderInitialHealth = TestInvaderInTowerRange.Health;
+
+
+                // When Tower fires on invaders
+                // with TestInvader as only Invader
+                TestTower.FireOnInvaders(new []{TestInvaderInTowerRange});
+
+                // Then invader's health should decrease
+                Assert.AreEqual(
+                    TestInvaderInTowerRange.Health,
+                    testInvaderInitialHealth - 1
+                );
+                // Then hit message should be printed
+                Assert.IsTrue(sw.ToString().Contains("is hit"));
+            }
+        }
     }
 }
