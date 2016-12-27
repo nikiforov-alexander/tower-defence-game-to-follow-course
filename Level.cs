c9b1bf138c0f3a08eb54ca8036960ffca86f7df8
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-12-27 16:26:00 +0100

    Add Level class with initial properties
    Towers and Invader, invaders have private settter and towers - public

---
+++ Level.cs
@@ -0,0 +1,11 @@
+﻿namespace TowerDefenceTreehouse
+{
+    public class Level
+    {
+
+        // properties
+        public Invader[] Invaders { get; private set; }
+        public Tower[] Towers { get; set; }
+
+    }
+}
