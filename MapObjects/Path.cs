99407f913175ffcfc2945d1c7537e97febbb4f2b
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2017-01-10 20:21:13 +0100

    Change pseudo JavaDoc to XML doc in onMap method
    in Path.cs

--- MapObjects/Path.cs
+++ MapObjects/Path.cs
@@ -15,14 +15,20 @@ namespace TowerDefenceTreehouse.MapObjects
 
         public int Length => _path.Length;
 
-        /**
-        Gets {@code MapLocation} at given {@code pathStep}.
-        @param pathStep : index of element in MapLocation array
-        {@code _path} field.
-        @return MapLocation
-        @throws OutOfBoundsException : when {@code pathStep}
-        is out of bounds from {@code _path} field.
-        */
+        /// <summary>
+        /// Gets <code>MapLocation</code> at given
+        /// <code>pathStep</code> : index of element in
+        /// <code>MapLocation</code> array <code>_path</code> field.
+        /// </summary>
+        /// <param name="pathStep"></param>
+        /// <returns>
+        ///     MapLocation object at <code>pathStep</code>
+        ///     index of <code>_path</code>
+        /// </returns>
+        /// <exception cref="OutOfBoundsException">
+        ///     if <code>pathStep</code> is out of bounds of
+        ///     <code>_path</code> array.
+        /// </exception>
         public MapLocation GetLocationAt(int pathStep)
         {
             if (pathStep < 0 || pathStep > _path.Length - 1)
